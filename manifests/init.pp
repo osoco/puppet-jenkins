4013e588feabde1180e57ca72c57958673e4487c
deigote <diego@toharia.com>
2012-07-13 13:12:17 +0200

    Adapted to new tomcat module

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,18 +2,19 @@ class jenkins ($ensure = "installed", $jenkins_home = "/ebs/jenkins") {
 
     include tomcat
 
-    $tomcat_package = "$tomcat::$tomcat_package"
+    $tomcat_package = "$tomcat::install::tomcat_package_name"
+    $tomcat_user = "$tomcat::install::tomcat_user"
 
     include wget
 
     file { "$jenkins_home":
         ensure => "directory",
-        owner => "$tomcat_package",
-        group => "$tomcat_package",
+        owner => "$tomcat_user",
+        group => "$tomcat_user",
         mode => "0755"
     }       
 
-    file { '/usr/share/$tomcat_package/.jenkins':
+    file { "/usr/share/$tomcat_user/.jenkins":
       ensure => link,
       target => "$jenkins_home",
       require => File["$jenkins_home"]
@@ -22,8 +23,7 @@ class jenkins ($ensure = "installed", $jenkins_home = "/ebs/jenkins") {
     wget::fetch { "jenkins-latest-war-download":
         source => "http://mirrors.jenkins-ci.org/war/latest/jenkins.war",
         destination => "/var/lib/$tomcat_package/webapps/jenkins.war",
-        require => [File['/usr/share/$tomcat_package/.jenkins'], Package["$tomcat_package"]],
-        notify => Service["$tomcat_package"]
+        require => [File["/usr/share/$tomcat_package/.jenkins"], Package["$tomcat_package"]],
     }
 
 }
